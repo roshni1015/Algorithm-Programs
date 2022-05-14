c4bad9ff0e59058f58dc13f5c6d98f9a45d76728
roshni1015 <roshniadatrao1110@gmail.com>
2022-05-14 17:46:30 +0530

    [Roshni-Anagram

---
+++ AlgorithmPrograms/AlgorithmPrograms/AnagramDetection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class AnagramDetection
+    {
+        public bool AnagramDetector(string First, string Second)
+        {
+            if (First == null || Second == null)
+            {
+                Console.WriteLine("Please enter two strings!");
+                return false;
+            }
+            if (First.Length != Second.Length)
+            {
+                Console.WriteLine("Please enter two strings of same length!");
+                return false;
+            }
+            char[] FirstCharArray = First.ToLower().ToCharArray();
+            char[] SecondCharArray = Second.ToLower().ToCharArray();
+            Array.Sort(FirstCharArray);
+            Array.Sort(SecondCharArray);
+            for (int i = 0; i < FirstCharArray.Length; i++)
+            {
+                if (FirstCharArray[i] != SecondCharArray[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
+        
+    
--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AlgorithmPrograms;
+
 Console.WriteLine("Welcome to Algorithm Programs");
+AnagramDetection anagram = new();
+Console.WriteLine("Please Enter the Two Strings to Find if They are Anagrams or not. :");
+string first = Console.ReadLine();
+string second = Console.ReadLine();
+
+if (anagram.AnagramDetector(first, second))
+{
+    Console.WriteLine("Given words are Anagrams!");
+}
+else
+{
+    Console.WriteLine("No! Given words are not Anagrams!");
+}
