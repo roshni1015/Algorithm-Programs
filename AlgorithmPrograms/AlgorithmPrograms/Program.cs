dceb823e72033fc4b8e681aad6b15ecb551544b6
roshni1015 <roshniadatrao1110@gmail.com>
2022-05-16 19:25:30 +0530

    [Roshni]-Merge Sort

--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -4,7 +4,7 @@ using AlgorithmPrograms;
 Console.WriteLine("Welcome to Algorithm Programs");
 while (true)
 {
-    Console.WriteLine("\nPlease choose the option :\n1) Anagram \n2) Binary Search \n3) Insertion Sort \n4) Bubble Sort \n5) Finding Prime Numbers");
+    Console.WriteLine("Please choose the option :\n1) Anagram \n2) Binary Search \n3) Insertion Sort \n4) Bubble Sort \n5) Finding Prime Numbers \n6) Merge sort");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -57,6 +57,25 @@ while (true)
         case 5:
             PrimeNumber p1 = new();
             p1.Primenumber();
+            break;
+        case 6:
+            MergeSort merge = new MergeSort();
+            int n = 8;
+            int[] array = { 35, 6, 50, 22, 45, 95, 67, 11, 75, 47 };
+            Console.WriteLine("Merge Sort");
+            Console.Write("Initial array is: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            merge.MergeSort1(array, 0, n - 1);
+            Console.Write("\nSorted Array is: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("*****************************************************************************");
             break;
         default:
             Console.WriteLine("Please choose the correct ");
